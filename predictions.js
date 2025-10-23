1076622cdf3a13ba5aa9c7d6a3be455185f8dd75
atomlinson <100231969+atomlinsonc@users.noreply.github.com>
2025-10-23 12:06:45 -0500

    Add files via upload

---
+++ predictions.js
@@ -0,0 +1,170 @@
+// NBA JAM Predictions Data
+// Update this file with your predictions from the spreadsheet
+// Teams should be ordered 1-15 for each conference
+
+const predictions = {
+    aaron: {
+        eastern: [
+            "Boston Celtics",
+            "New York Knicks", 
+            "Cleveland Cavaliers",
+            "Philadelphia 76ers",
+            "Milwaukee Bucks",
+            "Orlando Magic",
+            "Indiana Pacers",
+            "Miami Heat",
+            "Atlanta Hawks",
+            "Detroit Pistons",
+            "Chicago Bulls",
+            "Toronto Raptors",
+            "Charlotte Hornets",
+            "Brooklyn Nets",
+            "Washington Wizards"
+        ],
+        western: [
+            "Oklahoma City Thunder",
+            "Denver Nuggets",
+            "Minnesota Timberwolves",
+            "Dallas Mavericks",
+            "Phoenix Suns",
+            "Los Angeles Lakers",
+            "Golden State Warriors",
+            "Sacramento Kings",
+            "Los Angeles Clippers",
+            "Houston Rockets",
+            "Memphis Grizzlies",
+            "New Orleans Pelicans",
+            "San Antonio Spurs",
+            "Utah Jazz",
+            "Portland Trail Blazers"
+        ]
+    },
+    austin: {
+        eastern: [
+            "Boston Celtics",
+            "New York Knicks",
+            "Cleveland Cavaliers",
+            "Milwaukee Bucks",
+            "Philadelphia 76ers",
+            "Orlando Magic",
+            "Indiana Pacers",
+            "Miami Heat",
+            "Atlanta Hawks",
+            "Detroit Pistons",
+            "Chicago Bulls",
+            "Toronto Raptors",
+            "Charlotte Hornets",
+            "Brooklyn Nets",
+            "Washington Wizards"
+        ],
+        western: [
+            "Oklahoma City Thunder",
+            "Denver Nuggets",
+            "Minnesota Timberwolves",
+            "Dallas Mavericks",
+            "Los Angeles Lakers",
+            "Phoenix Suns",
+            "Golden State Warriors",
+            "Sacramento Kings",
+            "Los Angeles Clippers",
+            "Houston Rockets",
+            "Memphis Grizzlies",
+            "New Orleans Pelicans",
+            "San Antonio Spurs",
+            "Utah Jazz",
+            "Portland Trail Blazers"
+        ]
+    },
+    paul: {
+        eastern: [
+            "Boston Celtics",
+            "New York Knicks",
+            "Cleveland Cavaliers",
+            "Philadelphia 76ers",
+            "Milwaukee Bucks",
+            "Orlando Magic",
+            "Indiana Pacers",
+            "Miami Heat",
+            "Atlanta Hawks",
+            "Detroit Pistons",
+            "Chicago Bulls",
+            "Toronto Raptors",
+            "Charlotte Hornets",
+            "Brooklyn Nets",
+            "Washington Wizards"
+        ],
+        western: [
+            "Oklahoma City Thunder",
+            "Denver Nuggets",
+            "Minnesota Timberwolves",
+            "Dallas Mavericks",
+            "Phoenix Suns",
+            "Los Angeles Lakers",
+            "Golden State Warriors",
+            "Sacramento Kings",
+            "Los Angeles Clippers",
+            "Houston Rockets",
+            "Memphis Grizzlies",
+            "New Orleans Pelicans",
+            "San Antonio Spurs",
+            "Utah Jazz",
+            "Portland Trail Blazers"
+        ]
+    },
+    gill: {
+        eastern: [
+            "Boston Celtics",
+            "New York Knicks",
+            "Cleveland Cavaliers",
+            "Milwaukee Bucks",
+            "Philadelphia 76ers",
+            "Orlando Magic",
+            "Indiana Pacers",
+            "Miami Heat",
+            "Atlanta Hawks",
+            "Detroit Pistons",
+            "Chicago Bulls",
+            "Toronto Raptors",
+            "Charlotte Hornets",
+            "Brooklyn Nets",
+            "Washington Wizards"
+        ],
+        western: [
+            "Oklahoma City Thunder",
+            "Denver Nuggets",
+            "Minnesota Timberwolves",
+            "Dallas Mavericks",
+            "Phoenix Suns",
+            "Los Angeles Lakers",
+            "Golden State Warriors",
+            "Sacramento Kings",
+            "Los Angeles Clippers",
+            "Houston Rockets",
+            "Memphis Grizzlies",
+            "New Orleans Pelicans",
+            "San Antonio Spurs",
+            "Utah Jazz",
+            "Portland Trail Blazers"
+        ]
+    }
+};
+
+// INSTRUCTIONS FOR UPDATING PREDICTIONS:
+// 1. Open your Google Spreadsheet with the predictions
+// 2. For each player (Aaron, Austin, Paul, Gill):
+//    - Replace the Eastern Conference teams in order from 1-15
+//    - Replace the Western Conference teams in order from 1-15
+// 3. Make sure team names match exactly as shown above
+// 4. Save the file and commit to your GitHub repository
+// 
+// Team names must match exactly (including capitalization):
+// Eastern Conference Teams:
+// - Atlanta Hawks, Boston Celtics, Brooklyn Nets, Charlotte Hornets, Chicago Bulls
+// - Cleveland Cavaliers, Detroit Pistons, Indiana Pacers, Miami Heat, Milwaukee Bucks
+// - New York Knicks, Orlando Magic, Philadelphia 76ers, Toronto Raptors, Washington Wizards
+//
+// Western Conference Teams:
+// - Dallas Mavericks, Denver Nuggets, Golden State Warriors, Houston Rockets, Los Angeles Clippers
+// - Los Angeles Lakers, Memphis Grizzlies, Minnesota Timberwolves, New Orleans Pelicans
+// - Oklahoma City Thunder, Phoenix Suns, Portland Trail Blazers, Sacramento Kings
+// - San Antonio Spurs, Utah Jazz
